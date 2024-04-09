33489cb304532d4169ff4c8c9735e83fc24ab8b0
Nikkae <eneanika04@gmail.com>
2024-04-09 11:15:23 +0200

    Initial Commit

---
+++ prova git/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  prova git
+//
+//  Created by Enea Nika on 09/04/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
